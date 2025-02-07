1dabccc01a610b31b9ff03d667b67e439769aeda
Gagan <gaganc26101@gmail.com>
2025-02-07 15:25:31 +0530

    aml

---
+++ models/aml.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type AmlUserData struct {
+	gorm.Model
+	UserID         string    `gorm:"not null" json:"user_id"`
+	Occupation     string    `json:"occupation"`
+	SourceOfIncome string    `json:"source_of_income"`
+	ScaleOfIncome  string    `json:"scale_of_income"`
+	TradingExp     string    `json:"trading_exp"`
+	TaxLiability   string    `json:"tax_liability"`
+	TermsCondition string    `json:"terms_condition"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
+}
